71a983a4c25750d2889438928e6799b0120c4f6c
vdi-terminal <vdi-terminal@EDU-WSB.local>
2025-03-23 11:01:02 +0100

    dodanie path

--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
-
+const path = require('path');
 const axios = require('axios')
 const express = require('express')
 const app = express()
 
 app.use(express.json())
 app.use(express.static(path.join(__dirname, "js")));
+
 
 
 app.post('/weather', (req, res) => {
